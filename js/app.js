0a24f0bb9986d5b37e0096156cc2cf9fefff7b1b
Mancio <32124947+GabrielMancio@users.noreply.github.com>
2025-06-11 13:14:34 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let lista = document.getElementById('lista-amigos');
+
+function adicionar() {
+    let nome = document.getElementById('nome-amigo').value;
+    lista = document.getElementById('lista-amigos');
+    if (nome == ''){
+        return;
+    }
+    if (lista.textContent != ''){
+        lista.textContent += ', ';
+    }
+    lista.textContent += nome;
+    document.getElementById('nome-amigo').value = '';
+}
+
+function sortear() {
+    let nomes = lista.textContent.split(', ');
+    nomes.sort(() => Math.random() - 0.5);
+    let listaSorteio = document.getElementById('lista-sorteio');
+    listaSorteio.innerHTML = '';
+
+    for (let i = 0; i < nomes.length; i++){
+        let amigo = nomes[i];
+        let sorteado = nomes[(i + 1) % nomes.length];
+        listaSorteio.innerHTML += `<p>${amigo} -> ${sorteado}</p>`;
+    }
+}
+
+function reiniciar() {
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
